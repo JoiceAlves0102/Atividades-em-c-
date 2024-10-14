51577e22f023dfffa84107e8221a6f1ae64b34c7
Joice Alves <Joice9648@gmail.com>
2024-10-14 17:00:15 -0300

    Adicionando primeira atividade : percorrendo arrays com c#

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+//array
+
+
+void percorreArray()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    int acumulador = 0;
+
+    for(int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Índice [{i}] = {idades[i]}");
+        acumulador++;   
+    }
+}
